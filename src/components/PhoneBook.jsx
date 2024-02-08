39011ffc2f1ef06443f289fe99bb1ad5fefd3ab2
Olexander1234 <105589389+Olexander1234@users.noreply.github.com>
2024-02-08 18:27:06 +0200

    zxvz

--- src/components/PhoneBook.jsx
+++ src/components/PhoneBook.jsx
@@ -1,24 +1,21 @@
 import { useDispatch, useSelector } from "react-redux";
-import { Contacts, getFilterValue } from "../redux/selectors";
-import { deleteContact, fetchContacts, filterContactsByName } from "redux/operation";
+import { selectContacts, selectFilterValue ,selectVisibleContacts,} from "../redux/selectors";
+import { deleteContact,  fetchContacts, filterContactsByName } from "redux/operation";
 import { Button, Title, Wrapper } from "./PhoneBook.styled";
 import { FormPhoneBook } from "./FormPhoneBook";
 import { useEffect, useState } from "react";
 
 export const PhoneBook = ()=>{
     const dispatch = useDispatch();
-  const contacts = useSelector(Contacts)
+  const contacts = useSelector(selectVisibleContacts)
   
-  const filterValue = useSelector(getFilterValue)
+  // const filterValue = useSelector(selectFilterValue)
 useEffect(()=>{
   dispatch(fetchContacts())
 }, [dispatch])
   
-  const visibleContacts =  contacts.filter((contact )=>{
-console.log(contact);
-    return  contact.name.toLowerCase().includes(filterValue)
-  })
-   console.log(visibleContacts);
+  
+
  
   const deletePhone = (contactId) => {
     dispatch(deleteContact(contactId));
@@ -31,7 +28,7 @@ console.log(contact);
     
       <Title>Contacts</Title>
       <ul>
-        {visibleContacts.map(({ id, name, number }) => {
+        {contacts.map(({ id, name, number }) => {
           return (
             <Title key={id}>
               <p>
--- src/redux/selectors.jsx
+++ src/redux/selectors.jsx
@@ -1,2 +1,14 @@
-export const Contacts = (state) => state.contacts.items
-export const getFilterValue = (state) => state.filter
+import { createSelector } from "@reduxjs/toolkit";
+
+export const selectContacts = (state) => state.contacts.items
+export const selectFilterValue = (state) => state.filter
+
+
+
+
+export const selectVisibleContacts = createSelector([selectContacts ,selectFilterValue ], (contacts, filterValue)=>{
+  return  contacts.filter((contact )=>{
+        console.log(contact);
+            return  contact.name.toLowerCase().includes(filterValue)
+          })
+}) 
